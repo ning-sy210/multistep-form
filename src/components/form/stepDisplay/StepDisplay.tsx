6ba5fe1065820163eb14a62877f1a746cbf55c99
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2024-01-07 15:05:41 +0800

    feat (responsive): StepDisplay

--- src/components/form/stepDisplay/StepDisplay.tsx
+++ src/components/form/stepDisplay/StepDisplay.tsx
@@ -1,28 +1,44 @@
 import { useAtomValue } from "jotai";
+
 import { stepAtom } from "../Form.atoms";
+import { steps } from "../Form.constants";
+
+import DesktopSidebarBgImg from "/images/bg-sidebar-desktop.svg";
 
 import "./StepDisplay.scss";
 
-type StepDisplayProps = {
-  numberOfSteps: number;
-};
-
-const StepDisplay = ({ numberOfSteps }: StepDisplayProps) => {
-  const step = useAtomValue(stepAtom);
+const StepDisplay = () => {
+  const currStep = useAtomValue(stepAtom);
 
   return (
-    <div className="flex step-display">
-      {Array.from(Array(numberOfSteps)).map((_, i) => {
-        const index = i + 1;
-        return (
-          <div
-            key={index}
-            className={`hvc step-bubble ${index === step ? "selected" : ""}`}
-          >
-            {index}
-          </div>
-        );
-      })}
+    <div className="hc step-display">
+      <img
+        src={DesktopSidebarBgImg}
+        title="desktop sidebar background image"
+        className="desktop-sidebar-bg-img"
+      />
+
+      <div className="flex steps-ctn">
+        {steps.map((step, i) => {
+          const index = i + 1;
+          return (
+            <div key={index} className="flex step">
+              <div
+                className={`hvc step-bubble ${
+                  index === currStep ? "selected" : ""
+                }`}
+              >
+                {index}
+              </div>
+
+              <div className="stack-sb step-text">
+                <p className="step-counter-text">Step {index}</p>
+                <p className="step-label">{step.label}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
